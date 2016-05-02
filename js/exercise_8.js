4fe212456fa4ab67175288bcce8362bd4b25337f
cfm2391 <charles.moser@aemcorp.com>
2016-05-02 17:02:43 -0400

    Updated exercise_8.js

--- js/exercise_8.js
+++ js/exercise_8.js
@@ -17,3 +17,36 @@ map.on('locationfound', function(e) {
 
 });
 map.locate({setView:true});
+var routeLine = L.mapbox.featureLayer().addTo(map);
+function getDirections(frm, to){
+     var jsonPayLoad = JSON.stringify({
+         locations: [
+           {lat: frm[1]  lon: frm[0]},
+           {lat: to[1], lon: to[0]}}
+         ],
+         costing: 'pedestrian',
+         units: 'miles'
+     })
+     $.ajax({
+         url: 'http://valhalla.mapzen.comn/route',
+         data: {
+             json: jsonPayLoad,
+             api_key: 'valhalla-gwtf3x2'
+         }
+     }).done(function(data){
+          var routeShape = polyLine.decode(data.trip.legs[0].shape);
+          routeLine.setGeoJSON({
+              type: 'Feature',
+              geometry: {
+                  type: 'LineString',
+                  coordinaes: routeShape
+              },
+              properties: {
+                   "stroke: "#ed23f1",
+                   "stroke-opacity": 0.8,
+                   "stroke-width": 8
+              }
+    
+          })    
+      })
+}
