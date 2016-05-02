e9082f16e2a3d72194fc5e60f84749e740b4f634
cfm2391 <charles.moser@aemcorp.com>
2016-05-02 15:08:58 -0400

    Update exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,10 +2,10 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.k6fj4l3f';
+var mapId = 'cmoser2.019i55ba';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+var accessToken = 'https://a.tiles.mapbox.com/v4/cmoser2.019i55ba/page.html?access_token=pk.eyJ1IjoiY21vc2VyMiIsImEiOiJTUnlSdklzIn0.fLdMmX-JC-xCuQuKK_AH4g#3/0.00/0.00';
 
 // Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
