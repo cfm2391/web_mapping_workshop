622541992c9cec489f56457b56f8a2fce1ff2485
cfm2391 <charles.moser@aemcorp.com>
2016-05-02 16:35:19 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -30,11 +30,6 @@ featureLayer.on('ready', function() {
       'marker-symbol': 'restaurant'
     }))
   });
-  
-  featureLayer.on('ready', function(){
-  this.eachLayer(function(layer){
-    layer.bindPopup('Welcome to ' + layer.feature.properties.name);
-  });
-});
+
   map.fitBounds(featureLayer.getBounds());
 });
